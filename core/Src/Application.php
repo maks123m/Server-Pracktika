3fc2e6ec069af33a34e5c639ae594da5fdba2b10
Lite <makssarandaev@gmail.com>
2026-04-19 21:49:29 +0700

    Middleware

--- core/Src/Application.php
+++ core/Src/Application.php
@@ -15,16 +15,16 @@ class Application
    private Capsule $dbManager;
    private Auth $auth;
 
-   public function __construct(Settings $settings)
-   {
-       $this->settings = $settings;
-       $this->route = new Route($this->settings->getRootPath());
-       $this->dbManager = new Capsule();
-       $this->auth = new $this->settings->app['auth'];
+public function __construct(Settings $settings)
+{
+   $this->settings = $settings;
+   $this->route = Route::single()->setPrefix($this->settings->getRootPath());
+   $this->dbManager = new Capsule();
+   $this->auth = new $this->settings->app['auth'];
 
-       $this->dbRun();
-       $this->auth::init(new $this->settings->app['identity']);
-   }
+   $this->dbRun();
+   $this->auth::init(new $this->settings->app['identity']);
+}
 
    public function __get($key)
    {
